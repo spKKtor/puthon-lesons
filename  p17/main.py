c241d9fa4af7579a72c66671e45ff7ec5b37af7f
spKKtor <stasiik0710@gmail.com>
2024-05-11 12:10:34 +0300

    17

---  p17/main.py
+++  p17/main.py
@@ -1,15 +1,25 @@
 import pygame
 import random
 
-SCREEN_WIDTH = 100
-SCREEN_HEIGHT = 100
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 540
+IMAGES_PATH = 'images/'
 pygame.init()
-self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 class Game:
+    background = None
+    game_run: bool = False
     def __init__(self):
         pass
+
+    def menu(self):
+        pass
+
+    def add_background(self):
+        self.background = pygame.image.load(IMAGES_PATH + 'bg_03.png')
+        screen.blit(self.background,(0, 0))
 
     def init(self):
         while True:
@@ -17,9 +27,20 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     break
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_s:
+                        self.run()
 
+            screen.fill((50, 50, 50))
+            pygame.display.update()
     def run(self):
-        pass
+
+        self.game_run =True
+        while self.game_run:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        break
 
 
 # Start game
