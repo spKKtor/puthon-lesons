395a74acfa79e90cc1d94746e2a989503e5a528e
spKKtor <stasiik0710@gmail.com>
2024-05-18 12:05:04 +0300

    17

---  p17/main.py
+++  p17/main.py
@@ -1,40 +1,98 @@
 import pygame
 import random
+import sys
 
-SCREEN_WIDTH = 960
-SCREEN_HEIGHT = 540
-IMAGES_PATH = 'images/'
+SCREEN_WIDTH = 900 # TODO: Розмір має змінитись (можливо)
+SCREEN_HEIGHT = 800 #
+IMAGES_PATH_MENU = 'images/menu/'
+IMAGES_PATH_BG = 'images/background/'
+IMAGES_PATH_PLAYER = 'images/'
+IMAGES_PATH_ENEMY = 'images/'
+FONTS_PATH = 'fonts/'
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
-class Game:
-    background = None
-    game_run: bool = False
+# Головне меню
+class MainMenu:
+    bg = None
+    panel = None
+
     def __init__(self):
-        pass
+        self.add_bg()
+        self.add_panel()
 
-    def menu(self):
-        pass
+    def add_bg(self):
+        img = pygame.image.load(IMAGES_PATH_MENU + 'bg-03.jpg')
+        self.bg = pygame.transform.scale(img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    def add_background(self):
-        self.background = pygame.image.load(IMAGES_PATH + 'bg_03.png')
-        screen.blit(self.background,(0, 0))
+    def add_panel(self):
+        panel = pygame.image.load(IMAGES_PATH_MENU + 'Window.png')
+        self.panel = pygame.transform.scale(panel, (300, 300))
+
+    def draw(self):
+        screen.blit(self.bg, (0, 0))
+        screen.blit(self.panel, (20, 20))
+        self.start_btn()
+        self.exit_btn()
+
+
+    def start_btn(self):
+        font = pygame.font.Font(FONTS_PATH + 'PoetsenOne-Regular.ttf', 52)
+        text = font.render('START', True, (0, 0, 0))
+        screen.blit(text, (87, 100))
+
+    def exit_btn(self):
+        font = pygame.font.Font(FONTS_PATH + 'PoetsenOne-Regular.ttf', 52)
+        text = font.render('EXIT', True, (0, 0, 0))
+        screen.blit(text, (87, 150))
+
+    # TODO: ЗРОБИШ КЛІК МИШКИ
+    def click(self):
+        pos = pygame.mouse.get_pos()
+        btn = pygame.mouse.get_pressed()
+
+        if btn[0] and (pos[0]):
+            pass
+
+#Задній фон
+class Background:
+    img = None
+
+    def __init__(self):
+        self.add()
+
+    def add(self):
+        self.img = pygame.image.load(IMAGES_PATH_BG + 'bg_03.png')
+
+    def draw(self):
+        screen.blit(self.img, (0, 0))
+
+
+class Game:
+    game_run: bool = False
+
+    def __init__(self):
+        self.menu = MainMenu()
+        self.bg = Background()
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
 
     def init(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    break
-                if event.type == pygame.KEYDOWN:
+                    self.quit()
+                elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_s:
                         self.run()
 
-            screen.fill((50, 50, 50))
+            self.menu.draw()
             pygame.display.update()
-    def run(self):
 
+    def run(self):
         self.game_run =True
         while self.game_run:
             for event in pygame.event.get():
